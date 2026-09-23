6c77d445fa940ca7bbe11c2fffe51cfc8683113f
Nathan Myers <nathanemyers@gmail.com>
2026-09-23 17:50:54 -0500

    add links

--- src/InfoCard.tsx
+++ src/InfoCard.tsx
@@ -4,6 +4,7 @@ import { TEXT_COLOR } from "./colors"
 const Card = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 0.5rem;
   color: ${TEXT_COLOR};
   font-family: system-ui, sans-serif;
@@ -21,6 +22,14 @@ const Tagline = styled.p`
   opacity: 0.8;
 `
 
+const Link = styled.a``
+
+const Links = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+`
+
 interface InfoCardProps {
   className?: string
 }
@@ -29,7 +38,23 @@ export default function InfoCard(props: InfoCardProps) {
   return (
     <Card className={props.className}>
       <Name>Nathan Myers</Name>
-      <Tagline>Software Engineer</Tagline>
+      <Tagline>Software Engineer -- Chicago</Tagline>
+      <Links>
+        <Link
+          href="https://github.com/nathanemyers"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/nathan-myers-173a9719/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </Link>
+      </Links>
     </Card>
   )
 }
